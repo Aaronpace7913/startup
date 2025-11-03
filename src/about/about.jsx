d81020342aa5c0b314e29468b10fb6a38ecf43dc
Aaron <aaronpace7913@gmail.com>
2025-11-03 09:05:30 -0700

    feat: Update About component to fetch random images and quotes from APIs; enhance dynamic content loading

--- src/about/about.jsx
+++ src/about/about.jsx
@@ -6,15 +6,31 @@ export function About() {
   const [quote, setQuote] = React.useState('Loading...');
   const [quoteAuthor, setQuoteAuthor] = React.useState('unknown');
 
-    React.useEffect(() => {
-      setImageUrl(`img for cs260 startup.jpg`);
-      setQuote('Words are cheap; Show me the code');
-      setQuoteAuthor('Linus Torvalds');
-    }, []);
+  React.useEffect(() => {
+    const random = Math.floor(Math.random() * 1000);
+    fetch(`https://picsum.photos/v2/list?page=${random}&limit=1`)
+      .then((response) => response.json())
+      .then((data) => {
+        // Just use a fixed size instead of querying the DOM
+        const apiUrl = `https://picsum.photos/id/${data[0].id}/400/300?grayscale`;
+        setImageUrl(apiUrl);
+      })
+      .catch((error) => console.error('Error fetching image:', error));
+
+    fetch('https://quote.cs260.click')
+      .then((response) => response.json())
+      .then((data) => {
+        setQuote(data.quote);
+        setQuoteAuthor(data.author);
+      })
+      .catch((error) => console.error('Error fetching quote:', error));
+  }, []);
   
   return (
     <main>
-      <div className="picture-box"><img width="400px" src={imageUrl} alt="random" /></div>
+      <div className="picture-box">
+        <img width="400px" src={imageUrl} alt="random" />
+      </div>
       <div className="about-content">
         <p>
           GroupTask is a collaborative to-do list application that transforms individual task management into a team effort. 
@@ -27,9 +43,8 @@ export function About() {
         </p>
       </div>
       
-  
       <div id="quote">
-          <p><em>🔗 3rd Party API: Random Quotes Service</em></p>
+        <p><em>🔗 3rd Party API: Random Quotes Service & Picsum Photos</em></p>
         <div className="quote-text">{quote}</div>
         <div className="quote-author">{quoteAuthor}</div>
       </div>
