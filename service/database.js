366a7679d0a80cdf5e19f047f6c6161da9d81cbc
Aaron <aaronpace7913@gmail.com>
2025-11-11 11:03:37 -0700

    feat: Enhance database.js with user functions and connection testing

--- service/database.js
+++ service/database.js
@@ -11,3 +11,33 @@ const projectCollection = db.collection('project');
 const taskCollection = db.collection('task');
 const messageCollection = db.collection('message');
 const activityCollection = db.collection('activity');
+
+(async function testConnection() {
+  try {
+    await db.command({ ping: 1 });
+    console.log(`Connect to database`);
+  } catch (ex) {
+    console.log(`Unable to connect to database with ${url} because ${ex.message}`);
+    process.exit(1);
+  }
+})();
+
+// User functions
+function getUser(email) {
+  return userCollection.findOne({ email: email });
+}
+
+function getUserByToken(token) {
+  return userCollection.findOne({ token: token });
+}
+
+async function addUser(user) {
+  await userCollection.insertOne(user);
+}
+
+async function updateUser(user) {
+  await userCollection.updateOne(
+    { email: user.email }, 
+    { $set: user });
+}   
+
